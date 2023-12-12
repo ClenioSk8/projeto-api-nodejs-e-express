f950daf26fda3d1c62e46a67d4e3a03cdd42a693
Clênio Everson <126211546+ClenioSk8@users.noreply.github.com>
2023-12-12 20:32:34 -0300

    parou em 44:21 (https://www.youtube.com/watch?v=Cdu0WJhI-d8&t=1244s&ab_channel=ManualdoDev)

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,5 +1,10 @@
-const getAll = (req, res)=>{
-    return res.status(200).json({menssagem: ' Controler Funcionando'})
+const tasksModel = require('../models/tasksModel')
+
+const getAll = async (req, res)=>{
+    
+    const tasks = await tasksModel.getall()
+
+    return res.status(200).json(tasks)
 }
 
 module.exports = {
